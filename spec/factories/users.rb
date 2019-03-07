5ee16bec6ea35b8c3df6647cac635cf5cd01190d
fvh_P <ko_kurihara@yahoo.co.jp>
2019-03-08 00:35:03 +0900

    fix syntax for FactoryBot 5

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :user do
-    provider 'mastodon'
-    uid 'test@example.com'
-    email 'test@example.com'
-    password '12345678'
+    provider { 'mastodon' }
+    uid { 'test@example.com' }
+    email { 'test@example.com' }
+    password { '12345678' }
 
     trait :shizuka do
-      uid 'shizuka@example.com'
-      email 'shizuka@example.com'
+      uid { 'shizuka@example.com' }
+      email { 'shizuka@example.com' }
     end
 
     trait :chihaya do
-      uid 'chihaya@example.com'
-      email 'chihaya@example.com'
+      uid { 'chihaya@example.com' }
+      email { 'chihaya@example.com' }
     end
 
     trait :empty_email do
-      email ''
+      email { '' }
     end
   end
 end
